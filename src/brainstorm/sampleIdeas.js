83be8161c023694cb2199310c3b4478bafe85692
Santhosh Sundararaman <santhoshsram@gmail.com>
2020-12-10 20:43:50 +0530

    move sampleIdeas to its own file. we can remove it later

---
+++ src/brainstorm/sampleIdeas.js
@@ -0,0 +1,51 @@
+export const sampleIdeas = [
+  {
+    ideaId: "idea-1",
+    ideaContent: "This is an idea",
+    creator: "alpha@aarvalabs.com",
+    assignee: "alpha@aarvalabs.com",
+    comments: [
+      {
+        commenter: "beta@aarvalabs.com",
+        comment: "nice idea"
+      },
+      {
+        commenter: "charlie@aarvalabs.com",
+        comment: "let me make it a better idea, by adding sprinkling some magic"
+      }
+    ]
+  },
+  {
+    ideaId: "idea-2",
+    ideaContent:
+      "This is a really long idea. Let us see if there is space to fit this idea in the sticky.",
+    creator: "beta@aarvalabs.com",
+    assignee: "beta@aarvalabs.com",
+    comments: [
+      {
+        commenter: "alpha@aarvalabs.com",
+        comment: "nice idea"
+      },
+      {
+        commenter: "charlie@aarvalabs.com",
+        comment: "let me make it a better idea, by adding sprinkling some magic"
+      }
+    ]
+  },
+  {
+    ideaId: "idea-3",
+    ideaContent: "Ideas are dime a dozen, execution is what matters.",
+    creator: "charlie@aarvalabs.com",
+    assignee: "charlie@aarvalabs.com",
+    comments: [
+      {
+        commenter: "alpha@aarvalabs.com",
+        comment: "nice idea"
+      },
+      {
+        commenter: "beta@aarvalabs.com",
+        comment: "let me make it a better idea, by adding sprinkling some magic"
+      }
+    ]
+  }
+]
