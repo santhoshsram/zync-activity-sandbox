8fcd4bd408216a60530697b3d42fcf6f8002b0cb
Santhosh Sundararaman <santhoshsram@gmail.com>
2020-12-10 16:37:29 +0530

    added some jquery magic to show the modal properly in split view

--- src/brainstorm/Idea.jsx
+++ src/brainstorm/Idea.jsx
@@ -4,20 +4,56 @@ import $ from "jquery"
 const Idea = ({ idea, deleteIdeaHandler }) => {
   useEffect(() => {
     /*
-    If I make any change to the modal content and close it,
+    Problem 1: 
+      Below is a very ugly hack to handle how bootstrap shows modal
+    and modal-backdrop.
+    
+    Modal-backdrop is an empty, translucent div that bootstrap renders
+    behind the modal to blur the background. Bootstrap adds the
+    modal-backdrop as a direct child of the "body" element in the DOM.
+
+    However, bootstrap renders the modal itself as a child of the element
+    (usually div) in which it is defined. **There in lies the problem that
+    needs this ugly hack.**
+
+    When sandbox is in split view, each span (left and right) element's
+    position is fixed and it's z-Index is 1. All of the span element's
+    children, including the modal, inherit these properties. Consequently
+    the modal is rendered behind the modal-backdrop, and other div elements
+    in some cases.
+
+    Fix for Problem 1:
+      To fix this, below code does the following.
+
+      1. When the modal is displayed, make it a child of "body" element in
+         the DOM
+      2. When the modal is hidden (closed), make it a child of its original
+         parent. This is important. If we don't do this, then react will throw
+         an error when it tries to clean up the DOM and does not find the modal
+         under its original parent.
+
+    Problem 2:
+        If I make any change to the modal content and close it,
     the changes are preserved when I open the model the next time.
 
-    To avoid this we need to reset the contents of the form in
-    the modal so that if it is closed and reopened, the original
-    content is shown in the form.
+    Fix for Problem 2:
+      To avoid this we need to reset the contents of the form when the modal
+    is hidden (closed) so that next time it is opened, the original content is
+    shown in the form.
     */
+
+    $(`#editIdeaModal-${idea.id}`).on("shown.bs.modal", function () {
+      $(this).appendTo("body")
+    })
+
     $(`#editIdeaModal-${idea.id}`).on("hidden.bs.modal", function () {
       $(this).find("form").trigger("reset")
+      $(this).appendTo(`#ideaContainer-${idea.id}`)
     })
   }, [idea.id])
 
   return (
-    <>
+    <div id={`ideaContainer-${idea.id}`}>
       <div
         className="card border pb-0 m-1 bg-dark text-light"
         data-toggle="modal"
@@ -91,7 +127,7 @@ const Idea = ({ idea, deleteIdeaHandler }) => {
           </div>
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
