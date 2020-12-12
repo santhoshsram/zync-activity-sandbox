6d4f92ba368133272ac0f0a82a2fdddb3b02fb3f
Santhosh Sundararaman <santhoshsram@gmail.com>
2020-12-12 11:43:53 +0530

    minor refactor to AddNewIdea. prevent adding empty string

--- src/brainstorm/AddNewIdea.jsx
+++ src/brainstorm/AddNewIdea.jsx
@@ -1,9 +1,17 @@
 import React, { useState } from "react"
+import { addIdea } from "./BrainstormActions"
 
 const ENTER_KEY_CHAR_CODE = 13
 
 const AddNewIdea = ({ onAddClicked }) => {
   const [inputValue, setInputValue] = useState("")
+
+  const addIdeaHandler = () => {
+    if (inputValue !== "") {
+      onAddClicked(inputValue)
+      setInputValue("")
+    }
+  }
 
   return (
     <div className="mb-3 m-2">
@@ -16,8 +24,7 @@ const AddNewIdea = ({ onAddClicked }) => {
           onChange={(event) => setInputValue(event.target.value)}
           onKeyPress={(event) => {
             if (event.charCode === ENTER_KEY_CHAR_CODE) {
-              onAddClicked(event.target.value)
-              setInputValue("")
+              addIdeaHandler()
             }
           }}
         />
@@ -25,10 +32,7 @@ const AddNewIdea = ({ onAddClicked }) => {
           <button
             className="btn btn-primary"
             type="button"
-            onClick={() => {
-              onAddClicked(inputValue)
-              setInputValue("")
-            }}
+            onClick={addIdeaHandler}
           >
             Add Idea
           </button>
