9445b40362dc0d7d73fada82b9977accb547aa9e
Santhosh Sundararaman <santhoshsram@gmail.com>
2020-12-12 12:18:26 +0530

    add some useful helptext. remove unused imports from AddNewIdea component

--- src/brainstorm/AddNewIdea.jsx
+++ src/brainstorm/AddNewIdea.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react"
-import { addIdea } from "./BrainstormActions"
 
 const ENTER_KEY_CHAR_CODE = 13
 
--- src/brainstorm/RoundRobin.jsx
+++ src/brainstorm/RoundRobin.jsx
@@ -6,14 +6,23 @@ const RoundRobin = ({ user, ideas, roundRobinInfo, updateIdeaHandler }) => {
   const { userId } = user
   const usersCurIdxInQ = roundRobinInfo.idxInQ[userId]
   const reviewedUserId = roundRobinInfo.userIdQ[usersCurIdxInQ]
+  const curRound =
+    roundRobinInfo.userIdQ.length - (roundRobinInfo.roundsToGo + 1)
+
   return (
     <>
       {roundRobinInfo.roundsToGo > 0 ? (
-        <IdeasListing
-          ideas={ideasOfUser(ideas, reviewedUserId)}
-          deleteIdeaHandler={null}
-          updateIdeaHandler={updateIdeaHandler}
-        />
+        <>
+          <h3>Idea Review Round: {curRound}</h3>
+          <h5>
+            Add your comments / suggestions / improvements to the below ideas.
+          </h5>
+          <IdeasListing
+            ideas={ideasOfUser(ideas, reviewedUserId)}
+            deleteIdeaHandler={null}
+            updateIdeaHandler={updateIdeaHandler}
+          />
+        </>
       ) : (
         <h1>Round robins exhausted</h1>
       )}
