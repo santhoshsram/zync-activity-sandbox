6991c644b3eff316a41d4ad30b9568884c727d4f
Santhosh Sundararaman <santhoshsram@gmail.com>
2020-12-17 16:13:06 +0530

    fix bug in Switch component. for each switch tie the label and input elements together using a unique id (via the htmlFor property). before this change both the elements had a static id of "switch" and if multiple Switch components were created, all of them were controlling the same input element in the first Switch component

--- src/components/Switch.js
+++ src/components/Switch.js
@@ -1,19 +1,19 @@
 import React from "react"
 import "./switch.css"
 
-export const Switch = ({ checked, onChange }) => {
+export const Switch = ({ checked, onChange, id }) => {
   return (
     <div className="switch">
       <input
         checked={checked}
         onChange={onChange}
         className="switch-checkbox"
-        id="switch"
+        id={`switch-${id}`}
         type="checkbox"
       />
       <label
         className="switch-label"
-        htmlFor="switch"
+        htmlFor={`switch-${id}`}
         style={{ background: !checked && "#ccc" }}
       >
         <span className="switch-button" />
